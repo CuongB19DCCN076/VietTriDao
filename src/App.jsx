b867fd7dd01f8d12edfee8f94617f1a87279be47
Cuong <cuongchamchi1904@gmail.com>
2024-05-20 15:17:09 +0700

    aaaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Toaster } from 'react-hot-toast'
 import './App.css'
 import Root from './pages'
 
 function App() {
   return (
-    <>
+    <div>
       <Root />
-    </>
+      <Toaster />
+    </div>
   )
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,27 @@ import { Link } from "react-router-dom"
 import image1 from "../assets/image22.png";
 import image2 from "../assets/image23.png";
 import image3 from "../assets/image24.png";
+import toast from "react-hot-toast";
+import axios from "axios";
 const LoginForm = () => {
+    const fetchData = async () => {
+        try {
+            await axios.get("https://bookcar-backend-2.onrender.com/api/v1/auth/get-all-product-pagi/1")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const myPromise = fetchData();
+
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        toast.promise(myPromise, {
+        loading: 'Loading....',
+        success: 'Success',
+        error: 'Error when fetching',
+    });
+    }
     return (
         <section className="bg-white dark:bg-gray-900 h-[900px]">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto mt-10 lg:py-0">
@@ -26,7 +46,7 @@ const LoginForm = () => {
                                     <label htmlFor="" className="text-[30px] font-bold block">Password:</label>
                                     <input type="text" className="bg-[#f9e8e3] md:min-w-[424px] p-4 rounded-md" placeholder="Enter your password" />
                                 </div>
-                                <button className="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-[20px] font-bold text-white w-full mt-12">SIGN IN</button>
+                                <button className="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-[20px] font-bold text-white w-full mt-12" onClick={(e) => handleSubmit(e)}>SIGN IN</button>
                                 <Link to={"../../register"} className="text-skin-base text-[30px] font-bold block w-full text-center mt-8 " >
                                     Forgot your password?
                                 </Link>
