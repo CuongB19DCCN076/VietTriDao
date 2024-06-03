a89490280cfe345cfd921c29a429d9b72c11dc49
Cuong <cuongchamchi1904@gmail.com>
2024-06-03 15:49:31 +0700

    111

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	base: '/VietTriDao/', // Thay 'my-react-app' bằng tên repository của bạn
+	plugins: [react()]
+});
