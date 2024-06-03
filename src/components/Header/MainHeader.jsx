1913cbb38387ce9306bfbaed5af0106280c35943
Cuong <cuongchamchi1904@gmail.com>
2024-06-03 15:39:07 +0700

    update

--- src/components/Header/MainHeader.jsx
+++ src/components/Header/MainHeader.jsx
@@ -1,31 +1,54 @@
-import { Link } from 'react-router-dom'
-import logo from "./../../assets/logo.png"
-// import { useSelector } from 'react-redux';
-const MainHeader = () => {
-    // const theme = useSelector((state) => state.theme.value);
+import { Link } from "react-router-dom";
+import logo from "./../../assets/logo.png";
+
+import { CircleUserRound } from 'lucide-react';
+import { useEffect, useState } from "react";
+
+const Menu = () => {
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+    });
+
+    useEffect(() => {
+        const savedUser = JSON.parse(localStorage.getItem("user"));
+        setUser(savedUser);
+        console.log(savedUser)
+    }, []);
     return (
-        <div className='border-b-[3px] border-[#616060]'>
-            <div className='flex items-center px-14'>
-                <Link className='text-[23px] font-normal ' to={"../../../"}>
-                    <img src={logo} alt="logo" />
+        <div
+            className="z-[100]  h-[90px]  flex justify-between items-center px-[50px]  bg-[rgba(231,231,231,0.5)]
+		shadow-[0px_2px_4px_rgba(109,109,109,0.5)]"
+        >
+            {/* Left Menu */}
+            <div className="w-[30%] h-[80%]">
+                <Link to="">
+                    <img src={logo} alt="" className="h-[100%]" />
                 </Link>
-                <Link className='ml-2 w-[234px] flex justify-center items-center h-full text-[25px] font-bold text-[#616060]' to={"../../../"}>
-                    HomePage
-                </Link>
-                <div className='flex ml-auto'>
-                    <Link className=' flex justify-center items-center h-full text-[25px] mr-10 text-[#616060]' to={"../../../buy"}>
-                        BUY/RENT
-                    </Link>
-                    <Link className=' flex justify-center items-center h-full text-[25px] mr-10  text-[#616060]' to={"../../../sell"}>
-                        SELL
-                    </Link>
-                    <Link className=' flex justify-center items-center h-full text-[25px] mr-10  text-[#616060]' to={"../../../login"}>
-                        LOGIN
-                    </Link>
+            </div>
+            {/* Middle Menu */}
+            <ul className="flex w-[40%] justify-center gap-[80px] text-[20px]">
+                <li className="cursor-pointer">HOME</li>
+                <li className="cursor-pointer">BY/RENT</li>
+                <li className="cursor-pointer">SELL</li>
+            </ul>
+            {/* Right Menu */}
+            <div className="w-[30%] flex justify-end items-center">
+                <div className=" mr-[10px]">
+                    <CircleUserRound size={56} />
                 </div>
+                {user ? (
+                    <Link to="/login" className="text-red-500">
+                        user.email
+                    </Link>
+                ) : (
+                    <Link to="/login" className="text-red-500">
+                        Sign-in/Sign-up
+                    </Link>
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default MainHeader
+export default Menu;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,10 +10,12 @@ const Root = () => {
         {routes.map((item, index) => {
           const path = item.path;
           const Page = item.page;
+          // check element child
           const out = item?.outlet ? item?.outlet.map((itemOut, index) => {
             const PageOut = itemOut.page;
             return <Route path={itemOut.path} key={index} element={<PageOut />} />
           }) : null;
+          // check header 
           const Layout = item.isShowHeader ? Header : Fragment
           return <Route key={index} path={path} element={
             <Layout>
